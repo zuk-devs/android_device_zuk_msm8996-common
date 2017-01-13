af921dfc112a1098bd33955107b831f516a7fe5a
Myles Watson <mylesgw@google.com>
2017-01-13 11:47:17 -0800

    oneplus3: Build the Bluetooth HAL
    Test: VTS test passes on Marlin, Bluetooth starts/stops
    Change-Id: Ib1a1849c060c39e88c0219cb8421e3426cd3e7ac

--- device.mk
+++ device.mk
@@ -100,6 +100,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9330.xml:system/etc/sound_trigger_mixer_paths_wcd9330.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml 
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl
+    
 # Camera
 PRODUCT_PACKAGES += \
     Snap
