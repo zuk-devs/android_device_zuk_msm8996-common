8398d442d4fe5bced1ba9bf3f05bf7e6047b4d28
Santhosh Kumar Thimmanna Bhattar <sthim@codeaurora.org>
2017-05-09 19:27:39 +0530

    QCamera2: Disable QCameraDisplay temporarily
    Camera launch is failing due to QCameraDisplay crash.
    Disabling temporarily for AOSP upgrade to avoid the crash.
    This will be reverted once proper fix is found.
    
    Change-Id: I6ed87edcfbb07410899dcde13341b7ddd404960e

--- camera/QCamera2/HAL/QCamera2HWICallbacks.cpp
+++ camera/QCamera2/HAL/QCamera2HWICallbacks.cpp
@@ -728,9 +728,9 @@ void QCamera2HardwareInterface::synchronous_stream_cb_routine(
 
     frameTime = nsecs_t(frame->ts.tv_sec) * 1000000000LL + frame->ts.tv_nsec;
     // Calculate the future presentation time stamp for displaying frames at regular interval
-    if (pme->getRecordingHintValue() == true) {
+    /*if (pme->getRecordingHintValue() == true) {
         mPreviewTimestamp = pme->mCameraDisplay.computePresentationTimeStamp(frameTime);
-    }
+    }*/
     stream->mStreamTimestamp = frameTime;
     memory = (QCameraGrallocMemory *)super_frame->bufs[0]->mem_info;
 
