4c39288135d85a59c13e77f3f3a87554e38a165f
Muhammed Siju <msiju@codeaurora.org>
2017-06-23 17:41:19 +0530

    msm8996-common: QMI-FWK: Copy sec_config file correctly for combined images.
    Use TARGET_COPY_OUT_VENDOR makefile flag while copying sec_config
    file to copy to correct path irrespective of split/combined system
     vendor images.
    
    Change-Id: I77d9c53b1d59f51642c4a2d749739a9769577c09
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -164,7 +164,7 @@ PRODUCT_PACKAGES += \
 
 # IPC router config
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
+    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 	
 # Thermal
 PRODUCT_COPY_FILES += \
