63ef7705757b4578736a91686569c03c2007a3ec
davidevinavil <davidevinavil@gmail.com>
2017-06-30 18:41:43 +0200

    z2_plus: Move rootdir files to rootdir/etc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,7 +174,7 @@ BOARD_FLASH_BLOCK_SIZE := 262144
 TARGET_POWERHAL_VARIANT := qcom
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.qcom
 #TARGET_RECOVERY_UI_LIB := librecovery_ui_msm
 #TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_msm
 #TARGET_RELEASETOOLS_EXTENSIONS := device/qcom/common
