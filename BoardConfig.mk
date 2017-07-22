04edb7ca172a4707c5622c5bc7fac11fd4ceb22b
dd3boh <dade.garberi@gmail.com>
2017-07-22 13:49:47 +0200

    BoardConfig: Refactor

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,6 +35,14 @@ BOARD_VENDOR := zuk
 # Use Snapdragon LLVM, if available
 TARGET_USE_SDCLANG := true
 
+# SDclang
+LLVM_PREBUILTS_PATH := $(realpath $(TOP))/prebuilts/clang/linux-x86/arm-multiarch-linux-android-llvm-3.8/bin
+LLVM_RTLIB_PATH := $(realpath $(TOP))/prebuilts/clang/linux-x86/arm-multiarch-linux-android-llvm-3.8/lib/clang/3.8.2/lib/linux
+CLANG := $(LLVM_PREBUILTS_PATH)/clang
+CLANG_CXX := $(LLVM_PREBUILTS_PATH)/clang++
+LLVM_AS := $(LLVM_PREBUILTS_PATH)/llvm-as
+LLVM_LINK := $(LLVM_PREBUILTS_PATH)/llvm-link
+
 # Bootanimaion
 TARGET_BOOTANIMATION_MULTITHREAD_DECODE := true
 TARGET_BOOTANIMATION_PRELOAD := true
@@ -68,13 +76,6 @@ ENABLE_CPUSETS := true
 
 TARGET_USES_64_BIT_BINDER := true
 
-# SDclang
-LLVM_PREBUILTS_PATH := $(realpath $(TOP))/prebuilts/clang/linux-x86/arm-multiarch-linux-android-llvm-3.8/bin
-LLVM_RTLIB_PATH := $(realpath $(TOP))/prebuilts/clang/linux-x86/arm-multiarch-linux-android-llvm-3.8/lib/clang/3.8.2/lib/linux
-CLANG := $(LLVM_PREBUILTS_PATH)/clang
-CLANG_CXX := $(LLVM_PREBUILTS_PATH)/clang++
-LLVM_AS := $(LLVM_PREBUILTS_PATH)/llvm-as
-LLVM_LINK := $(LLVM_PREBUILTS_PATH)/llvm-link
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=tty60,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=16M@0-0xffffffff
@@ -137,7 +138,6 @@ TARGET_CAMERASERVICE_CLOSES_NATIVE_HANDLES := true
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
-BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
 
 # CM Hardware
 BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/cmhw
