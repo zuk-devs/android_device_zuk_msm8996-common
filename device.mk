45095db024e5e722c57af56dd4902ff3a0a91c7f
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:17:09 +0200

    oneplus3: Build Vibrator HIDL HAL
    Change-Id: I3353060afd4f7b63ebd94a3a46d40cb7e651d8b5

--- device.mk
+++ device.mk
@@ -260,6 +260,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+
 # WiFi HAL
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
