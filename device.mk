7edc9e81319a1c3e122453203ab4fc36625b9406
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:11:27 +0200

    oneplus3: Build Camera HIDL HAL
    Change-Id: Id845e815310027d0feb60c83b487f776d9175fa1

--- device.mk
+++ device.mk
@@ -107,6 +107,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@3.2-impl \
     Snap
 
 # Connectivity Engine support (CNE)
