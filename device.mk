e1dc735eb17ae039570f86e3f1e84ceb84f3e070
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 14:01:07 -0700

    z2_plus: Move Lineage specific overlays to overlay-lineage
    Change-Id: Ic38bb1acb4d37f90b62679e4420815f49b30fc7f

--- device.mk
+++ device.mk
@@ -25,7 +25,9 @@
 $(call inherit-product-if-exists, vendor/zuk/z2_plus/z2_plus-vendor.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+    #$(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
