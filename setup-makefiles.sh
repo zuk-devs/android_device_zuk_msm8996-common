4b2b978d0f6690a73bc91e82001e13c69b4af797
Bruno Martins <bgcngm@gmail.com>
2017-08-23 14:43:23 +0100

    z2_plus: Update extractors to use Lineage vendor
     * Match the branding change as well
    
    Change-Id: Ie39f8f52f3a7d552c82c0da643732af447aeb91e
    Signed-off-by: dd3boh <dade.garberi@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -30,9 +30,9 @@ INITIAL_COPYRIGHT_YEAR=2017
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -40,7 +40,7 @@ fi
 . "$HELPER"
 
 # Initialize the helper for common
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" true
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" true
 
 # Copyright headers and guards
 write_headers "z2_plus"
@@ -54,7 +54,7 @@ write_footers
 if [ -s "$MY_DIR"/../"$DEVICE"/proprietary-files.txt ]; then
     # Reinitialize the helper for device
     INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-    setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT" false
+    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false
 
     # Copyright headers and guards
     write_headers
