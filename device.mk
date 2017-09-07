5343f430e2623a6a7a9737f04150e933a7283900
dd3boh <dade.garberi@gmail.com>
2017-09-07 13:42:31 +0200

    Revert "AudioFX: Fix force close"
    This reverts commit 155acf2b047152e5d886884e886031574c25bd37.

--- device.mk
+++ device.mk
@@ -91,6 +91,7 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
