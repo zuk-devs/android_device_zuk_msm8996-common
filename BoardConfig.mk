f51c9e51ed245148ed30e8a158ef66bcd66649b4
davide <davidevinavil@gmail.com>
2017-09-13 01:48:16 +0200

    ril: Switch to caf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -171,6 +171,9 @@ TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.qcom
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
+# Ril
+TARGET_RIL_VARIANT := caf
+
 # SELinux
 # include device/qcom/sepolicy/sepolicy.mk
 # BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
