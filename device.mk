7d3687801d2c4b503807698e34a5a3dd881bfce4
dd3boh <dade.garberi@gmail.com>
2017-09-16 17:29:32 +0200

    z2_plus: Fix device not booting with aosp hals
    Signed-off-by: dd3boh <dade.garberi@gmail.com>

--- device.mk
+++ device.mk
@@ -139,6 +139,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
+    libgenlock \
     gralloc.msm8996 \
     hwcomposer.msm8996 \
     memtrack.msm8996
