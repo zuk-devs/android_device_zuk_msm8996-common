a2e51ae0ecfca82263b17a731b0bcd743f290e1c
dd3boh <dade.garberi@gmail.com>
2017-11-07 14:47:35 +0100

    msm8996-common: Update config.fs from LA.UM.6.5.r1-04000-8x96.0
    Signed-off-by: dd3boh <dade.garberi@gmail.com>

--- config.fs
+++ config.fs
@@ -30,3 +30,33 @@ mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
+
+[vendor/bin/xtwifi-client]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: NET_BIND_SERVICE BLOCK_SUSPEND
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[bt_firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[dsp/]
+mode: 0771
+user: AID_MEDIA
+group: AID_MEDIA
+caps: 0
