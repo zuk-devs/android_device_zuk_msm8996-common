f50b98a6d0513a8c795a451e9d7300cf5cecf2d4
dd3boh <dade.garberi@gmail.com>
2017-11-07 18:01:48 +0100

    Revert "msm8996-common: Move bin files to vendor [2/2]"
    This reverts commit a18ae7d8278f9be32946f3f67dbf0c49add82b31.

--- config.fs
+++ config.fs
@@ -1,37 +1,37 @@
 [AID_QTI_DIAG]
 value:2901 
 
-[system/vendor/bin/wcnss_filter]
+[system/bin/wcnss_filter]
 mode: 0755
 user: AID_BLUETOOTH
 group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND
 
-[system/vendor/bin/cnss-daemon]
+[system/bin/cnss-daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/vendor/bin/pm-service]
+[system/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/vendor/bin/imsdatadaemon]
+[system/bin/imsdatadaemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/vendor/bin/ims_rtp_daemon]
+[system/bin/ims_rtp_daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
-[vendor/bin/xtwifi-client]
+[system/bin/xtwifi-client]
 mode: 0755
 user:  AID_GPS
 group: AID_GPS
