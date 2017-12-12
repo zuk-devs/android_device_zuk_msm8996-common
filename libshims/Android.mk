efde9739f9382a17508ba389b7c0ee5b90e60f08
Davide Garberi <dade.garberi@gmail.com>
2017-12-12 20:04:00 +0100

    msm8996-common: Update GPS configuration for upstream
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -14,16 +14,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-# get_process_name shim
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := get_process_name.c
-
-LOCAL_MODULE := libshims_get_process_name
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
-
 # camera shim
 include $(CLEAR_VARS)
 
--- msm8996.mk
+++ msm8996.mk
@@ -149,18 +149,17 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    gps.msm8996 \
-    libcurl \
+    libgnss \
     libgnsspps \
-    libshims_get_process_name
+    libcurl
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/etc/lowi.conf:system/etc/lowi.conf \
-    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
-    $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
+    $(LOCAL_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
+    $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
 # Graphics
 PRODUCT_PACKAGES += \
