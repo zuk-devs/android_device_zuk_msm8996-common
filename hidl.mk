f7d0ad52b941a5ccc0fe21db455a90c161819349
Davide Garberi <dade.garberi@gmail.com>
2017-12-17 09:15:31 +0100

    msm8996-common: Nuke camera provider service
    * Seems like just slowing down the camera
    
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -34,8 +34,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-impl
 
 # Display
 PRODUCT_PACKAGES += \
