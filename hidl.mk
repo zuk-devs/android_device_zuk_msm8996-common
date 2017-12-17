89c3f653937ddf13771bf8b42125d2767375b9f6
Davide Garberi <dade.garberi@gmail.com>
2017-12-17 17:45:55 +0100

    Revert "msm8996-common: Nuke camera provider service"
    * This seemed like slowing down everything while using the old camera blobs, but now it just makes everything work faster
    
    This reverts commit f7d0ad52b941a5ccc0fe21db455a90c161819349.

--- hidl.mk
+++ hidl.mk
@@ -34,7 +34,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service
 
 # Display
 PRODUCT_PACKAGES += \
