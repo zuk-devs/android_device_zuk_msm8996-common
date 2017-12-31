bb70dc3240faa6b8f874a23071aacb2bde7d432a
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-31 04:40:40 +0200

    msm8996-common: Build QTI HIDL power 1.0 HAL
     * Despite zuk msm8996 devices have QCACLD2 WLAN card and the node that should
       return WiFi stats is available, it seems that the firmware
       doesn't fully supports it, causing freezes when trying to
       access the node. Just go with the HAL that provides no
       support for this feature.
    
    Change-Id: I929a2d3aedf3bfdca962dc34720019d83c7134e1
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -97,7 +97,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.0-service-qti
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
