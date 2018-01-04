eb370f44081b733e5c5a1896b0b4a7fe912d57ef
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-01-04 13:55:30 +0100

    msm8996: build libdisplayconfig and libhwc2on1adapter
    Change-Id: I089a74239d164e49b5bcca788cbca28f9fb0e819
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -126,6 +126,8 @@ PRODUCT_PACKAGES += \
     gralloc.msm8996 \
     hwcomposer.msm8996 \
     memtrack.msm8996 \
+    libdisplayconfig \
+    libhwc2on1adapter \
     libgenlock \
     liboverlay \
     libtinyxml
