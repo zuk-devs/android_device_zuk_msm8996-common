f057501b102b40c343d6f73f4ed33071fec16f79
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-01-06 21:04:06 +0100

    charger: Move to aosp configuration

--- hidl.mk
+++ hidl.mk
@@ -70,6 +70,13 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
+
+# Healthd
+PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-convert \
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-service
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
