bf5e7dbb793fd2a78d806409d9dd86eee083484b
davidevinavil <davidevinavil@gmail.com>
2018-01-08 22:54:41 +0100

    overlay: Remove lineage overlay

--- msm8996.mk
+++ msm8996.mk
@@ -29,8 +29,7 @@ $(call inherit-product, $(LOCAL_PATH)/hidl.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
