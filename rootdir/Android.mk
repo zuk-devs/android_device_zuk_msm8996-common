63cd3332b5ef56dd418ba105a7f517315f67b6fe
Davide Garberi <dade.garberi@gmail.com>
2018-01-10 22:53:21 +0100

    msm8996-common: rootdir: Move init scripts to vendor [1/2]
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE		:= fstab.qcom
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/fstab.qcom
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_VENDOR_MODULE	:= true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -15,7 +15,8 @@ LOCAL_MODULE		:= init.qcom.power.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.qcom.power.rc
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_VENDOR_MODULE	:= true
+LOCAL_MODULE_RELATIVE_PATH	:= init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -23,7 +24,8 @@ LOCAL_MODULE		:= init.qcom.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.qcom.rc
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_VENDOR_MODULE	:= true
+LOCAL_MODULE_RELATIVE_PATH	:= init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -31,37 +33,40 @@ LOCAL_MODULE		:= init.qcom.usb.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.qcom.usb.rc
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_VENDOR_MODULE	:= true
+LOCAL_MODULE_RELATIVE_PATH	:= init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.qcom.usb.sh
 LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
+LOCAL_MODULE_CLASS	:= EXECUTABLES
 LOCAL_SRC_FILES		:= etc/init.qcom.usb.sh
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_VENDOR_MODULE	:= true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.qcom.bt.sh
 LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
+LOCAL_MODULE_CLASS	:= EXECUTABLES
 LOCAL_SRC_FILES		:= etc/init.qcom.bt.sh
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_VENDOR_MODULE	:= true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.safailnet.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.safailnet.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE		:= init.safailnet.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.safailnet.rc
+LOCAL_VENDOR_MODULE	:= true
+LOCAL_MODULE_RELATIVE_PATH	:= init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.qcom.rc
+LOCAL_MODULE_STEM	:= ueventd.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/ueventd.qcom.rc
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
