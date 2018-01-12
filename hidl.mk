3c2fafd12513618cc19dcedcc23639de8e1d16d1
Bruno Martins <bgcngm@gmail.com>
2018-01-12 22:03:34 +0000

    msm8996-common: Build Lineage vibrator HAL
    Change-Id: Id60fb36cbae9f4a6dfb0cd926e7c05b798c5699d
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -117,8 +117,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.0-service.lineage
 
 # WiFi HAL
 PRODUCT_PACKAGES += \
