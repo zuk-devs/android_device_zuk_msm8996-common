0f57b53851eec381510412f2e38511dc483ba0f9
Davide Garberi <dade.garberi@gmail.com>
2018-01-20 17:02:20 +0100

    msm8996-common: Enable dexpreopt
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -112,6 +112,14 @@ BOARD_USES_QCNE := true
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
 
+# Dex
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    WITH_DEXPREOPT ?= true
+  endif
+endif
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
+
 # Display
 BOARD_USES_ADRENO := true
 TARGET_CONTINUOUS_SPLASH_ENABLED := true
