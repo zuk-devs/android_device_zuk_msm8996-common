9463e3b5a6e438610f6d9d9e8bc5c787a90184cd
davidevinavil <davidevinavil@gmail.com>
2018-01-21 23:15:28 +0100

    msm8996-common: wifi: Move readmac to vendor
    Change-Id: I6a370dfcf3346104880eaacfee6a5368d45a7dd7
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -2,10 +2,11 @@ LOCAL_PATH:= $(call my-dir)
 
 ifeq ($(strip $(BOARD_HAS_QCOM_WLAN)),true)
 include $(CLEAR_VARS)
-LOCAL_MODULE           := readmac
-LOCAL_MODULE_TAGS      := optional
-LOCAL_SRC_FILES        := zuk_readmac.c
-LOCAL_CFLAGS           += -Wall
-LOCAL_SHARED_LIBRARIES := libc libcutils libutils liblog
+LOCAL_MODULE             := readmac
+LOCAL_MODULE_TAGS        := optional
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_SRC_FILES          := zuk_readmac.c
+LOCAL_CFLAGS             += -Wall
+LOCAL_SHARED_LIBRARIES   := libc libcutils libutils liblog
 include $(BUILD_EXECUTABLE)
 endif
