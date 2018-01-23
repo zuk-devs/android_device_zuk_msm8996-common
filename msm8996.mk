9ff5325f2439b76eb6e508d1e0280524cb5b6b01
Davide Garberi <dade.garberi@gmail.com>
2018-01-23 22:56:51 +0100

    msm8996-common: Switch to Snap and enable api2
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -138,7 +138,7 @@ PRODUCT_PACKAGES += \
     camera.msm8996 \
     libmm-qcamera \
     mm-qcamera-app \
-    SnapdragonCamera \
+    Snap \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
