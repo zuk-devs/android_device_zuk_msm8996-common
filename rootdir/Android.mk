56fb6d7f48ed3eb66183c89df8b999843d095013
Cosme Domínguez Díaz <cosme.ddiaz@gmail.com>
2018-03-10 11:09:54 +0100

    msm8996-common: Remove init.qcom.bt.sh
      * We don't execute it.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -38,14 +38,6 @@ LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.qcom.bt.sh
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= EXECUTABLES
-LOCAL_SRC_FILES		:= etc/init.qcom.bt.sh
-LOCAL_MODULE_PATH  	:= $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.qcom.rc
 LOCAL_MODULE_STEM	:= ueventd.rc
 LOCAL_MODULE_TAGS	:= optional eng
