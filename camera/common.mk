0915876f8e64438e478c13365fedeb292caf9018
Cosme Domínguez Díaz <cosme.ddiaz@gmail.com>
2018-03-28 19:38:22 +0200

    msm8996-common: camera: Remove deprecated kernel header dependency path.

--- camera/common.mk
+++ camera/common.mk
@@ -2,6 +2,6 @@ common_deps :=
 kernel_includes :=
 
 ifeq ($(call is-vendor-board-platform,QCOM),true)
-    common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+    common_deps += INSTALLED_KERNEL_HEADERS
     kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 endif
