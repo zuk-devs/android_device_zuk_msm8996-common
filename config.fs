3f9c646583947dd36288b2b5086ad1a78a06ecbb
Cosme Domínguez Díaz <cosme.ddiaz@gmail.com>
2018-04-08 17:46:23 +0200

    msm8996-common: Add imsrcsd to config.fs.

--- config.fs
+++ config.fs
@@ -43,6 +43,11 @@ user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
+[vendor/bin/imsrcsd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+
 [vendor/bin/cnd]
 mode: 0755
 user: AID_SYSTEM
