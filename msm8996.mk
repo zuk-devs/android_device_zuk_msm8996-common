90582cb51039767113120c528a31d415dc540a8b
Rashed Abdel-Tawab <rashed@linux.com>
2018-04-15 13:58:07 -0700

    msm8996: Build libqdMetaData.system
    Change-Id: I29fb36cfc1c1298f31947bdd2d3cd8d07c22209a

--- msm8996.mk
+++ msm8996.mk
@@ -174,7 +174,8 @@ PRODUCT_PACKAGES += \
     libhwc2on1adapter \
     libgenlock \
     liboverlay \
-    libtinyxml
+    libtinyxml \
+    libqdMetaData.system \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/calib.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/calib.cfg
