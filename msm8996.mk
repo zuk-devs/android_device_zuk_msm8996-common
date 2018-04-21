8a865790db75f9716ad6ec0c7dbccd8997e576bd
Cosme Domínguez Díaz <cosme.ddiaz@gmail.com>
2018-04-22 01:07:07 +0200

    msm8996-common: Decommonize thermal-engine.conf.
      * z2_row and z2_plus have different thresholds.

--- msm8996.mk
+++ msm8996.mk
@@ -409,6 +409,3 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
