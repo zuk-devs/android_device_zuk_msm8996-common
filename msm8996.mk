1d6157b5346a3ff7bccfd7fd17be36481cc7bf3a
davidevinavil <davidevinavil@gmail.com>
2018-05-09 00:52:13 +0200

    msm8996-common: Remove duplicate package

--- msm8996.mk
+++ msm8996.mk
@@ -305,10 +305,6 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
-PRODUCT_PACKAGES += \
-    ipacm \
-    IPACM_cfg.xml
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
