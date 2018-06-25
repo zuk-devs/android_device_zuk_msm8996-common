d2cd1f396a80076b9e5dd13140e710bcba04b3e6
Cosme Domínguez Díaz <cosme.ddiaz@gmail.com>
2018-06-25 22:45:18 +0200

    msm8996-common: Move thermal-engine.conf from vendor to msm8996-common.

--- msm8996.mk
+++ msm8996.mk
@@ -400,3 +400,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
