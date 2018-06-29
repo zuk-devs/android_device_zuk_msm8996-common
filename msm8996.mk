168bdf60e45fa1ab9d4aed21e30c4f5ebfcd4252
LuK1337 <priv.luk@gmail.com>
2018-06-29 18:44:41 +0200

    msm8996-common: Remove camera.device@3.2-impl from PRODUCT_PACKAGES
    * camera.device@3.2-impl is a dependency of camera provider
      therefore there's no need to add it to PRODUCT_PACKAGES.
    
    Change-Id: I3ae211965a31b5f85af3dd0fa1c7811b802586c3

--- msm8996.mk
+++ msm8996.mk
@@ -132,7 +132,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    camera.device@3.2-impl \
     camera.msm8996 \
     libmm-qcamera \
     mm-qcamera-app \
