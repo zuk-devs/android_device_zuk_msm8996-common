d1f38b3713915d3a204bb29b231999d97b9a6298
Bruno Martins <bgcngm@gmail.com>
2018-08-11 01:07:49 +0100

    msm8996-common: Set vendor props via PRODUCT_PROPERTY_OVERRIDES
     * Support for vendor.prop was abandoned
    
    Change-Id: I3554bfc57c27555f8c605e8555ea5f386bce0c5d

--- msm8996.mk
+++ msm8996.mk
@@ -82,6 +82,9 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
+# Vendor properties
+-include $(LOCAL_PATH)/vendor_prop.mk
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
