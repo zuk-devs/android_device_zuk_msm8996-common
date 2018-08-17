87bcf72a4283c3973ae63f4b149272451aa01f1c
Bruno Martins <bgcngm@gmail.com>
2018-08-17 07:37:38 +0200

    msm8996-common: Use QTI bluetooth stack
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -127,8 +127,6 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service \
     libbt-vendor
 
 # Camera
