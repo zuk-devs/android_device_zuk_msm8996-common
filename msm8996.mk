04a7bd03b9aa144ba8efb505c3efc2a10b944ced
davidevinavil <davidevinavil@gmail.com>
2018-08-18 14:23:31 +0200

    Revert "msm8996-common: Use BOARD_VNDK_VERSION := current"
    This reverts commit e35c10e57b9df6454437d2d3b914771da79e5a46.
    
    Change-Id: Iec0cf241399ac609a131f3b298ad5ca1722ce715
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -183,7 +183,6 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libtinyxml \
     libqdMetaData.system \
-    libvulkan
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/calib.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/calib.cfg
@@ -366,7 +365,7 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_PACKAGES += \
-    vndk_package
+    vndk-sp
 
 # Vibrator
 PRODUCT_PACKAGES += \
