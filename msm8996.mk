ff1767abdadd491cea1562636a266b83266fbea8
MRTHAKER <mahek.haker@gmail.com>
2018-08-23 11:40:47 +0200

    msm8996-common: Add custom biometric hal From AOSP -inspired from mido godix hal -also includes 2.0-->2.1 wrapper code
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -382,7 +382,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.0-service.zuk
 
 # Thermal
 PRODUCT_PACKAGES += \
