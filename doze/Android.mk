dc3f5aa15292569a80d8e88d22093db59c8c4bbe
davidevinavil <davidevinavil@gmail.com>
2018-08-26 02:26:42 +0200

    msm8996-common: Build ZukDoze

--- doze/Android.mk
+++ doze/Android.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := ZukDoze
-LOCAL_SDK_VERSION := current
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
