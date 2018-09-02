d69abdcc0e2d6f642749d6a0966a53fef03a27f5
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-09-02 12:07:22 +0200

    msm8996-common: Don't build vndk_package
    * It's built by default if vndk is set to current
    
    Change-Id: I2a00446046bafe265e5ac894656abc72514319d8
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- msm8996.mk
+++ msm8996.mk
@@ -383,9 +383,6 @@ PRODUCT_VENDOR_VERITY_PARTITION=/dev/block/bootdevice/by-name/factory
 $(call inherit-product, build/target/product/verity.mk)
 
 # VNDK
-PRODUCT_PACKAGES += \
-    vndk_package
-
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v28/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v28.so \
     prebuilts/vndk/v28/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v28.so \
