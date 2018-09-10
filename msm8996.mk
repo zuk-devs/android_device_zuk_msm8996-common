f2f42a7f24af39edc58ebb66109f4e4fcd7e1334
Vol Zhdanov <wight554@gmail.com>
2018-09-10 18:42:52 +0000

    msm8996-common: Switch to power-libperfmgr
    * cleanup unused camera hints
    * revert b296bbf
    
    Change-Id: I6a52a8d0bd527b153dc9558670366b14667803e5
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -308,10 +308,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    android.hardware.power@1.2-service.zuk-libperfmgr
 
 # QCOM
 PRODUCT_COPY_FILES += \
