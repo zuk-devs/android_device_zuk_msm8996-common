777a1591a0ccc73b86f92152151ee61c6a73864c
Vol Zhdanov <wight554@gmail.com>
2018-09-10 18:42:52 +0000

    msm8996-common: Switch to power-libperfmgr
    * cleanup unused camera hints
    * also revert b296bbf
    
    Signed-off-by: Subhajeet Muhuri <kenny3fcb@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -304,7 +304,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service.zuk_msm8996-libperfmgr
 
 # QCOM
 PRODUCT_COPY_FILES += \
