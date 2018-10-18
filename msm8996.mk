63c452393792aab06887285aa254da50b9f2bfca
Demon Singur <demonsingur@gmail.com>
2018-10-18 10:38:04 +0000

    msm8996-common: remove qti perf stack
    Change-Id: I8b96175100db0ce5280ba91807a98188280c22db
    Signed-off-by: Subhajeet Muhuri <kenny3fcb@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -307,9 +307,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service-qti
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
-
 # QCOM
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
