fb2ab77a30cc7d6d5ea11e3d8631c218feaacade
Davide Garberi <dade.garberi@gmail.com>
2018-11-04 11:16:33 +0100

    msm8996-common: Switch back to vulkan 1.0
    * This might break the GPU frequency switch on some games
    
    Revert "Update Vulkan hardware level supported to 1"
    
    This reverts commit 91b4cb3452a2000424e4536e7e66adfebfe04670.
    
    Revert "msm8996-common: Advertise Vulkan 1.1 system feature"
    
    This reverts commit 52f63d31e49d21dd3f80acbb237d545de8642749.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -2,6 +2,10 @@
 # Common vendor properties for msm8996
 #
 
+# Adreno
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qcom.adreno.qgl.ShaderStorageImageExtendedFormats=0
+
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
