c171cbe4d779e4077b994aea6b47ec18e5dd5aa1
Subhajeet Muhuri <kenny3fcb@gmail.com>
2018-11-16 10:03:24 +0530

    msm8996-common: Disable interaction boost
     * To remove conflicts between EAS and powerhal's power profiles
       https://github.com/LineageOS/android_hardware_qcom_power/commit/40048116d86686c008146cbcc406bc94549351a4
    
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -173,7 +173,6 @@ TARGET_USES_MKE2FS := true
 # Power
 TARGET_HAS_NO_WLAN_STATS := true
 TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/touch/tp_dev/gesture_on"
-TARGET_USES_INTERACTION_BOOST := true
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
