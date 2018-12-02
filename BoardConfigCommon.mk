eb76ff33695050c5ed8932960f4a7c45f40c38ad
Davide Garberi <dade.garberi@gmail.com>
2018-12-02 22:11:55 +0100

    msm8996-common: power: Set the double tap to wake node in powerhal directly
    Change-Id: Iecff9931a5e400a51564e6190344e0ff66236df0
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -183,7 +183,6 @@ TARGET_USES_MKE2FS := true
 
 # Power
 TARGET_HAS_NO_WLAN_STATS := true
-TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/touch/tp_dev/gesture_on"
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
