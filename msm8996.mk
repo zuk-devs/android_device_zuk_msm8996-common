7e42202b40db0afa762df6530a94fffb99336d07
davidevinavil <davidevinavil@gmail.com>
2019-01-16 12:54:06 +0100

    msm8996-common: Use BOARD_VNDK_VERSION := current

--- msm8996.mk
+++ msm8996.mk
@@ -177,7 +177,8 @@ PRODUCT_PACKAGES += \
     libgenlock \
     liboverlay \
     libtinyxml \
-    libqdMetaData.system
+    libqdMetaData.system \
+    libvulkan
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/calib.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/calib.cfg
@@ -360,7 +361,7 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_PACKAGES += \
-    vndk-sp
+    vndk_package
 
 # Vibrator
 PRODUCT_PACKAGES += \
