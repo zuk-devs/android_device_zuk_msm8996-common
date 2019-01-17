9253423d3e09126d43006ddf873a4f15d20f1006
davidevinavil <davidevinavil@gmail.com>
2019-01-17 01:14:07 +0100

    msm8996-common: Build RRO

--- msm8996.mk
+++ msm8996.mk
@@ -30,6 +30,9 @@ $(call inherit-product, vendor/zuk/msm8996-common/msm8996-common-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
