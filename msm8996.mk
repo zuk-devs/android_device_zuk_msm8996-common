525c1979e3f245b2e26dc934c2b17702f84e9075
Davide Garberi <dade.garberi@gmail.com>
2019-01-19 18:40:39 +0100

    msm8996-common: power-libperfmgr: Rename to zuk_msm8996
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    Change-Id: I7c7040850bf748059302e88bf02e9f98f8a56c79
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -312,7 +312,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.zuk-libperfmgr
+    android.hardware.power@1.2-service.zuk_msm8996-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
--- power-libperfmgr/service.cpp
+++ power-libperfmgr/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@1.2-service.zuk-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.2-service.zuk_msm8996-libperfmgr"
 
 #include <android/log.h>
 #include <hidl/HidlTransportSupport.h>
