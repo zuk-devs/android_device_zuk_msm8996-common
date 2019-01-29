LineageOS/android_device_xiaomi_msm8996-common

msm8996-common: Add MATRIX file for vendor HAL's
Change-Id: I6c7eed54f066e61ad8039eef5ee6081e6de43560

BoardConfigCommon.mk
@@ -139,6 +139,7 @@ TARGET_EXFAT_DRIVER := sdfat
 TARGET_FS_CONFIG_GEN := $(VENDOR_PATH)/config.fs
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(VENDOR_PATH)/framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(VENDOR_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(VENDOR_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(VENDOR_PATH)/compatibility_matrix.xml

zuk-devs/android_device_zuk_msm8996-common

BoardConfigCommon.mk
@@ -158,6 +158,7 @@ USE_DEVICE_SPECIFIC_GPS := true
 OVERRIDE_QCOM_HARDWARE_VARIANT := msm8998
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(VENDOR_PATH)/vendor_framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(VENDOR_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(VENDOR_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(VENDOR_PATH)/compatibility_matrix.xml