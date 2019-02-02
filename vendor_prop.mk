45cc7df55fe981c4e768ebbcad9a7b5161417cdf
LuK1337 <priv.luk@gmail.com>
2019-02-02 19:34:23 +0100

    msm8996-common: Add debug IMS props to workaround CarrierConfig restrictions
    We support IMS features on carriers not listed in the CarrierConfig,
    so let's use the props to enable it everywhere.
    
    Change-Id: I54b48b4196908b9c11e8aa781a05600a8f380a35

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -151,7 +151,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.apm_sim_not_pwdn=1 \
     persist.vendor.radio.sib16_support=1 \
     persist.vendor.radio.custom_ecc=1 \
-    persist.vendor.radio.rat_on=combine
+    persist.vendor.radio.rat_on=combine \
+    persist.radio.calls.on.ims=1 \
+    persist.dbg.ims_volte_enable=1
 
 # RmNet Data
 PRODUCT_PROPERTY_OVERRIDES += \
