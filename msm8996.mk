d31f23040057d4efa6e508bf25daa194ad8cefb4
davidevinavil <davidevinavil@gmail.com>
2019-05-08 15:50:17 +0200

    msm8996-common: Remove ZukPref

--- msm8996.mk
+++ msm8996.mk
@@ -404,10 +404,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-	
-#Zuk Extras
-PRODUCT_PACKAGES += \
-    ZukPref	
 
 # WiFi Display
 PRODUCT_PACKAGES += \
