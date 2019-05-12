1208709d346b06b444ec17775229afccf5b696ea
Davide Garberi <dade.garberi@gmail.com>
2019-05-12 11:55:09 +0200

    msm8996-common: Update blobs to LA.UM.7.5.r1-04800-8x96.0
    Change-Id: I45f4e3576bf59da3a510962cbd0293b9890991a1

--- msm8996.mk
+++ msm8996.mk
@@ -332,6 +332,8 @@ PRODUCT_PACKAGES += \
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.0 \
+    android.hardware.radio.deprecated@1.0 \
+    android.hardware.radio@1.1 \
     android.hardware.secure_element@1.0 \
     librmnetctl \
     libxml2
