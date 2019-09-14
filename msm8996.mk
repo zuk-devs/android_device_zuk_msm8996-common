5db023f42c7a6b118a33cac2637387f95fb3f57c
Davide Garberi <dade.garberi@gmail.com>
2019-09-15 00:31:21 +0200

    [DNM] disable lineage overlays
    Change-Id: Ib0014b5d6bbd561f89c0734e3b0bc0acc556adca

--- msm8996.mk
+++ msm8996.mk
@@ -28,8 +28,8 @@ $(call inherit-product, vendor/zuk/msm8996-common/msm8996-common-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
+#    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
