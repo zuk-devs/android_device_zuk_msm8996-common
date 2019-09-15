b6e91cd6b9c03d45515c5f15e9184d23d02f9d84
Davide Garberi <dade.garberi@gmail.com>
2019-09-15 17:25:18 +0200

    msm8996-common: Switch to thermal 2.0
    Change-Id: Icb0223aa27866e17ddb1c6763ed1ec15f6b3f0c0

--- msm8996.mk
+++ msm8996.mk
@@ -419,8 +419,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@2.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
