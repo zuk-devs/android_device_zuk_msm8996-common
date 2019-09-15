6a5b2daafd3e5e2aa29dc35c96134001733d7528
Davide Garberi <dade.garberi@gmail.com>
2019-09-15 16:56:12 +0200

    msm8996-common: Build thermal.msm8996
    Change-Id: Ie8441190cee8177179cbef815cffd53aeb0d09a7

--- msm8996.mk
+++ msm8996.mk
@@ -419,7 +419,8 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@1.0-service \
+    thermal.msm8996
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
