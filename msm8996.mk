a19f416c90fdbf1bf4a6013dd6b66f73b2dc02a9
dianlujitao <dianlujitao@lineageos.org>
2019-09-16 17:40:51 +0800

    msm8996-common: Create system copy of ims-ext-common and add to classpath
     * Our ims.apk doesn't load ims-ext-common.jar, and it's moved to /product
       on 10 where PRODUCT_BOOT_JARS doesn't work.
     * Create a system copy of the jar and add it to boot classpath to make
       it loadable from the apk without extra work,
    
    Change-Id: Ia530a530bd4469e12500410fc5e8ba393de60b18

--- msm8996.mk
+++ msm8996.mk
@@ -356,9 +356,11 @@ PRODUCT_PACKAGES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    ims-ext-common_system \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
+    ims-ext-common_system \
     telephony-ext
 
 # TextClassifier
