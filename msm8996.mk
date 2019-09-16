133a0fcea3aec4fd92e324e415ca038123784184
Davide Garberi <dade.garberi@gmail.com>
2019-09-16 14:50:46 +0200

    msm8996-common: Import biometrics interface from android-10.0.0_r2
    Change-Id: I42aec640e146b54a433886460e7002ed3b6a248e

--- msm8996.mk
+++ msm8996.mk
@@ -415,7 +415,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service \
+    android.hardware.biometrics.fingerprint@2.1-service.zuk_msm8996 \
     ZukPocketMode
 
 # Thermal
