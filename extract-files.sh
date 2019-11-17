f47dde6de034d4d3f7e9d59b23f2be1ab21acdd7
Davide Garberi <dade.garberi@gmail.com>
2019-11-17 21:34:47 +0100

    msm8996-common: Update vulkan blobs from LA.UM.8.6.r1-01900-89xx.0
    * Older 8x96 blobs break vulkan with updated graphics
    
    Change-Id: I0556b1adf6a62720602f6a4c4994423adb789b1c

--- extract-files.sh
+++ extract-files.sh
@@ -97,6 +97,11 @@ function blob_fixup() {
     vendor/lib64/lib-dplmedia.so)
         patchelf --remove-needed libmedia.so "${2}"
         ;;
+
+    # Rename vulkan.msm8953
+    vendor/lib/hw/vulkan.msm8996.so | vendor/lib64/hw/vulkan.msm8996.so)
+        patchelf --set-soname "vulkan.msm8996.so" "${2}"
+        ;;
     esac
 }
 
