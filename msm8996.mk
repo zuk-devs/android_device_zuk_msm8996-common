33342dcf37ae7baefb3f0bec6f337451dc53cf30
Mikhail Naganov <mnaganov@google.com>
2019-11-22 10:56:29 -0800

    msm8996-common: Switch to Audio HAL V6
    Bug: 141989952
    Test: atest VtsHalAudioV6_0TargetTest
          atest VtsHalAudioEffectV6_0TargetTest
                manual audio "smoke" test
    
    Change-Id: Ieb1cc5c7fdcc5416a328a0a5c4a1996258b81ac8

--- msm8996.mk
+++ msm8996.mk
@@ -106,9 +106,9 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@5.0-impl \
+    android.hardware.audio@6.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     audio.a2dp.default \
     audio.primary.msm8996 \
     audio.r_submix.default \
