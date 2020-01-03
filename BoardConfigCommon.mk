3672150b9b228c136eecfe0a992f8c88382cf59b
kubersharma001 <kubersharma001@gmail.com>
2020-01-03 14:51:16 +0100

    Revert "msm8996: cryptfs: wait for qseecomd"
    This reverts commit 807a47e9d1934449c272ab933df6772e7987ae5e.
    
    Change-Id: I97b73679928aabb227be536165ab412b91a67dd7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -134,7 +134,6 @@ TARGET_ENABLE_MEDIADRM_64 := true
 
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
-TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
 # Exclude serif fonts for saving system.img size.
 EXCLUDE_SERIF_FONTS := true
