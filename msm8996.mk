8aa074c94c24bda30a2cdbba5a81a4bca1068c84
Davide Garberi <dade.garberi@gmail.com>
2020-04-03 14:09:39 +0200

    msm8996-common: Add ANT+ back
    * Also switch to OnePlus5 blobs since ZUK's don't work correctly
      and daisy ones lack ANT+ support
    
    Change-Id: I69c7c7883aad8eb1d32347d3871a0cc0ed36c6c3

--- msm8996.mk
+++ msm8996.mk
@@ -93,6 +93,14 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService \
+    com.dsi.ant.antradio_library
+
+PRODUCT_COPY_FILES += \
+    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
