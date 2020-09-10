ed3f539e157530177d5b4470f5a293e9165f4da3
Davide Garberi <dade.garberi@gmail.com>
2020-09-10 14:04:52 +0200

    [TMP] msm8996-common: Use AOSP's power 1.0
    Change-Id: I920b8801b32188b505d643309363665cf95e46d3

--- msm8996.mk
+++ msm8996.mk
@@ -329,7 +329,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
