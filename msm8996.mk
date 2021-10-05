316736aa646eb6e01dc753dc4ca9c22c75159039
Davide Garberi <dade.garberi@gmail.com>
2021-10-05 15:40:02 +0200

    msm8996-common: Exclude LineageParts overlays from RRO
    Change-Id: I8f95de6be002767d7f281cfea38147ac29da9d66

--- msm8996.mk
+++ msm8996.mk
@@ -35,6 +35,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/packages/apps/LineageParts
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
