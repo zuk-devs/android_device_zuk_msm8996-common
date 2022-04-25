113c981f6697f77757c33b46c5bf1ee8c8c0ef92
Sal Savage <salsavage@google.com>
2022-04-25 16:42:02 -0700

    msm8996-common: Set default the Bluetooth class of device
    The class of device is a string with a list of uint8t values:
    90,2,12
    
    The meaning is as follows:
    {Service Field, Major class, Minor class}
    
    Service Field: 0x5A -> 90
        Bit 17: Networking
        Bit 19: Capturing
        Bit 20: Object Transfer
        Bit 22: Telephony
    MAJOR_CLASS: 0x02 -> 2 (Phone)
    MINOR_CLASS: 0x0C -> 12 (Smart Phone)
    
    Bug: 217452259
    Test: make -j; -- check that bluetooth.device.class_of_device is
    correct
    
    Merged-In: I70301be5e19f2cbc39b626621a62bc529974ca56
    Change-Id: I70301be5e19f2cbc39b626621a62bc529974ca56

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -49,6 +49,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.bt.bdaddr_path=/mnt/vendor/persist/bluetooth/bt_mac
 
 PRODUCT_PRODUCT_PROPERTIES += \
+    bluetooth.device.class_of_device=90,2,12 \
     bluetooth.hardware.power.operating_voltage_mv=3300 \
     bluetooth.profile.a2dp.source.enabled?=true \
     bluetooth.profile.avrcp.target.enabled?=true \
