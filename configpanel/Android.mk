1b1490596ee30e84222cc5e55074c64945b01e83
Davide Garberi <dade.garberi@gmail.com>
2022-08-26 22:58:51 +0200

    msm8996-common: configpanel: Remove androidx preference_preference
    Change-Id: I4e92806ce0f3e8ae83365696cfba90c7a96ac9d2

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -13,7 +13,6 @@ LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.core_core \
-    androidx.preference_preference \
     org.lineageos.settings.resources
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
