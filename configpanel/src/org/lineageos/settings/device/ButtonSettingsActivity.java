f567d770db3257c2c8c94f1b9a2eca55bae6e2da
Bruno Martins <bgcngm@gmail.com>
2022-12-06 17:59:00 +0000

    msm8996-common: configpanel: Fix compilation for QPR1 merge
    Change-Id: Ifa602ed1ae0734dbda40663770f2e54075bfa208

--- configpanel/src/org/lineageos/settings/device/ButtonSettingsActivity.java
+++ configpanel/src/org/lineageos/settings/device/ButtonSettingsActivity.java
@@ -20,7 +20,7 @@ package org.lineageos.settings.device;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.collapsingtoolbar.R;
+import com.android.settingslib.widget.R;
 
 public class ButtonSettingsActivity extends CollapsingToolbarBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
